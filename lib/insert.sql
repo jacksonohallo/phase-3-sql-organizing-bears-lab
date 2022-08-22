4950cfbc946e5593d547d5a2b0f0972a4266c857
jacksonohallo <jacksonohallo@gmail.com>
2022-08-22 12:24:24 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Mr. Chocolate',23,'M','black','Phlegmatic',1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Rowdy',3,'M','black','Phlegmatic',0);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Tabitha',31,'F','black','Phlegmatic',1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Sergeant Brown',23,'M','white','Phlegmatic',1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Melisa',21,'F','black','Phlegmatic',0);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Grinch',18,'M','white','Phlegmatic',1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ('Wendy',56,'F','black','Phlegmatic',0);
+INSERT INTO  bears (name,age,sex,color,temperament,alive)
+VALUES ( Null,11,'F','black','Phlegmatic',1);
